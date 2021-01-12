f41f532f580555a576a35fa317c3f024deb6b587
luthfiyahridwan <76866255+luthfiyahridwan@users.noreply.github.com>
2021-01-12 12:08:43 +0700

    Tugas-1

---
+++ Assignment #1 - Basic Python [nomor 1].py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+nama= input("nama saya:")
+
+
+# In[2]:
+
+
+umur= input("umur saya:")
+
+
+# In[3]:
+
+
+tinggi= input ("tinggi saya:")
+
+
+# In[4]:
+
+
+print("nama saya adalah "+ nama, "umur saya " + umur, "tahun", "tinggi badan saya "+tinggi , 'cm')
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Assignment #1 - Basic Python [nomor 2].py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[65]:
+
+
+Pi = 3.14
+r = float(input(' Jari-jari lingkaran: '))
+luas = Pi * r* r
+
+
+# In[66]:
+
+
+print(f'Luas lingkaran dengan jari-jari {r} cm adalah {luas} cm\u00b2')
+
+
+
+
+
+
